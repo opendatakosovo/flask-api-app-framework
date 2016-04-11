69231563501364e08ae101d2f76c0973f28f0299
georges <georges@opendatakosovo.org>
2016-04-11 20:01:28 +0200

    tweak

--- import.py
+++ import.py
@@ -11,7 +11,7 @@ def run_importer():
     # This is to expand the app's context(scope),
     # so that we can use mongo configurations for the importer too.
     with app.app_context():
-      DaataImporter().run()
+      DataImporter().run()
 
 if __name__ == '__main__':
     run_importer()
