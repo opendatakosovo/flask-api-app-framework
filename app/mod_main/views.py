eee0fce5f3aa3694c2caa61c8c5d6e7fa27c9cda
KosovareSahatqija <kosovare.sahatqija@gmail.com>
2016-08-15 11:59:05 +0200

    Added templates for the Feed and SignUp page

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,11 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 mod_main = Blueprint('main', __name__)
 
 
-@mod_main.route('/', methods=['GET'])
-def index():
+@mod_main.route('/sign', methods=['GET'])
+def sign():
     ''' Renders the App index page.
     :return:
     '''
-    return "Welcome to the Flask App."
+    return render_template('mod_main/signUp.html')
+
+@mod_main.route('/', methods=['GET'])
+@mod_main.route('/feed', methods=['GET'])
+def feed():
+    ''' Renders the App index page.
+    :return:
+    '''
+    return render_template('mod_feed/feed.html')
